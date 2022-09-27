7d049c6f8c2689fe2d56457e880d4bc4793838c6
Michał Królikowski <Michal.Krolikowski@rossmann.com.pl>
2022-09-27 11:45:51 +0200

    Add project files.

---
+++ UnitTestTraining.Test/UnitTest1.cs
@@ -0,0 +1,73 @@
+using NUnit.Framework;
+using System;
+
+namespace UnitTestTraining.Test
+{
+    [Parallelizable(ParallelScope.Children)]
+    public class Tests
+    {
+        [TestCase("")]
+        public void TestStringCalculatedEmptyString(string numbers)
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add(numbers);
+            Assert.That(() => result, Is.EqualTo(0));
+        }
+
+        [TestCase("1")]
+        public void TestStringCalculatedTest1(string numbers)
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add(numbers);
+            Assert.That(() => result, Is.EqualTo(1));
+        }
+
+        [TestCase("1,2")]
+        public void TestStringCalculatedTest2(string numbers)
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add(numbers);
+            Assert.That(() => result, Is.EqualTo(3));
+        }
+
+        [TestCase("1\n2,3")]
+        public void TestStringCalculatedTest3(string numbers)
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add(numbers);
+            Assert.That(() => result, Is.EqualTo(6));
+        }
+
+        [TestCase("-1,2,3")]
+        public void TestStringCalculatedTest4(string numbers)
+        {
+            var calc = new StringCalculator();
+            Assert.That(() => calc.Add(numbers), Throws.TypeOf<ArgumentException>());
+        }
+
+        [TestCase("2,1001")]
+        public void TestStringCalculatedTest5(string numbers)
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add(numbers);
+            Assert.That(() => result, Is.EqualTo(2));
+        }
+
+        [TestCase("//[***]\n1***2***3")]
+        public void TestStringCalculatedTest6(string numbers)
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add(numbers);
+            Assert.That(() => result, Is.EqualTo(6));
+        }
+
+        [TestCase("//[*][%]\n1*2%3")]
+        public void TestStringCalculatedTest7(string numbers)
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add(numbers);
+            Assert.That(() => result, Is.EqualTo(6));
+        }
+
+    }
+}
---
+++ UnitTestTraining/StringCalculator.cs
@@ -0,0 +1,39 @@
+﻿namespace UnitTestTraining
+{
+    public class StringCalculator
+    {
+        //"//[***]\n1***2***3"
+        public int Add(string number)
+        {
+            if (string.IsNullOrEmpty(number)) return 0;
+
+            List<string> stringList = new List<string>();
+
+            if (number.StartsWith("//"))
+            {
+
+                //Delimiter
+                string[] inputLines = number.Replace("//", "").Split('\n');
+
+                string[] delimiters = inputLines[0].Replace("]", "").Split('[');
+
+                stringList = inputLines[1].Split(delimiters, System.StringSplitOptions.RemoveEmptyEntries).ToList();
+            }
+            else
+            {
+                char[] delimiter = { ',', '\n' };
+                stringList = number.Split(delimiter).ToList();
+            }
+
+            if(stringList.Where(a=> Convert.ToInt32(a) < 0).Any())
+            {
+                string message = $"Negatives not allowed: {String.Join(", ", stringList.Where(a => Convert.ToInt32(a) < 0).Select(s => s))}";
+                throw new ArgumentException(message);
+            }
+
+            stringList.RemoveAll(a => Convert.ToInt32(a) > 1000);
+
+            return stringList.Select(s=> Convert.ToInt32(s)).Sum(a=>a);
+        }
+    }
+}
