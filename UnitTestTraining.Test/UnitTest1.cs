7726de09004c5316402613ba8e2cc57bbb151187
Michał Królikowski <Michal.Krolikowski@rossmann.com.pl>
2022-09-27 13:45:45 +0200

    Podpiecie testow do statycznej metody

--- UnitTestTraining.Test/UnitTest1.cs
+++ UnitTestTraining.Test/UnitTest1.cs
@@ -9,63 +9,55 @@ namespace UnitTestTraining.Test
         [TestCase("")]
         public void TestStringCalculatedEmptyString(string numbers)
         {
-            var calc = new StringCalculator();
-            var result = calc.Add(numbers);
+            var result = StringCalculator.Add(numbers);
             Assert.That(() => result, Is.EqualTo(0));
         }
 
         [TestCase("1")]
         public void TestStringCalculatedTest1(string numbers)
         {
-            var calc = new StringCalculator();
-            var result = calc.Add(numbers);
+            var result = StringCalculator.Add(numbers);
             Assert.That(() => result, Is.EqualTo(1));
         }
 
         [TestCase("1,2")]
         public void TestStringCalculatedTest2(string numbers)
         {
-            var calc = new StringCalculator();
-            var result = calc.Add(numbers);
+            var result = StringCalculator.Add(numbers);
             Assert.That(() => result, Is.EqualTo(3));
         }
 
         [TestCase("1\n2,3")]
         public void TestStringCalculatedTest3(string numbers)
         {
-            var calc = new StringCalculator();
-            var result = calc.Add(numbers);
+            var result = StringCalculator.Add(numbers);
             Assert.That(() => result, Is.EqualTo(6));
         }
 
         [TestCase("-1,2,3")]
         public void TestStringCalculatedTest4(string numbers)
         {
-            var calc = new StringCalculator();
-            Assert.That(() => calc.Add(numbers), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => StringCalculator.Add(numbers), Throws.TypeOf<ArgumentException>());
         }
 
         [TestCase("2,1001")]
         public void TestStringCalculatedTest5(string numbers)
         {
-            var calc = new StringCalculator();
-            var result = calc.Add(numbers);
+            var result = StringCalculator.Add(numbers);
             Assert.That(() => result, Is.EqualTo(2));
         }
 
         [TestCase("//[***]\n1***2***3")]
         public void TestStringCalculatedTest6(string numbers)
         {
-            var calc = new StringCalculator();
-            var result = calc.Add(numbers);
+            var result = StringCalculator.Add(numbers);
             Assert.That(() => result, Is.EqualTo(6));
         }
 
         [TestCase("//[*][%]\n1*2%3")]
         public void TestStringCalculatedTest7(string numbers)
         {
-            var calc = new StringCalculator();
-            var result = calc.Add(numbers);
+            var result = StringCalculator.Add(numbers);
             Assert.That(() => result, Is.EqualTo(6));
         }
 
